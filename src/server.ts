e1f2498312839f3821f8a54270fa22883be87220
Matheus Filipe <mfcr031@gmail.com>
2020-04-16 16:00:59 -0300

    start

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello world" });
+app.get('/', (req, res) => {
+  return res.json({ message: 'hello world' });
 });
 
 app.listen(3333);
